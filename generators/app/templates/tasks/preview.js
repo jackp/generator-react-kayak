bdcb672e48e75190262c0f687faaf66424666c10
Jack Parker <parkej3@gmail.com>
2015-06-12 15:45:06 -0400

    Build dist bundle before previewing

--- generators/app/templates/tasks/preview.js
+++ generators/app/templates/tasks/preview.js
@@ -5,7 +5,7 @@ var gulp = require("gulp");
 var path = require("path");
 var webserver = require("gulp-webserver");
 
-gulp.task("preview", function() {
+gulp.task("preview", ["build"], function() {
   gulp.src(path.resolve(process.cwd(), "dist"))
     .pipe(webserver({
       port: 3001,
